8af21e1b0406909aa7fd117ca016735ca6fc323a
Ethan Tom <ethan@planet.com>
2019-12-11 16:06:21 -0800

    Added social media icons

---
+++ client/src/pages/Social.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import EmailIcon from '@material-ui/icons/Email';
+
+class Social extends Component {
+    render() {
+        return (
+//            TODO add links to each profile
+            <GitHubIcon/>
+            <LinkedInIcon/>
+            <InstagramIcon/>
+            <EmailIcon/>
+        )
+    }
+}
+
+export default Social;
--- client/src/pages/Work.js
+++ client/src/pages/Work.js
@@ -11,7 +11,29 @@ class Work extends Component {
       },
     });
     render() {
+//    TODO use MUI Expansion Panels for work and Cards for Projects
 
+//Responsibilities:
+//
+//Design, develop and maintain automated test cases
+//Take ownership of testing features and automation till release.
+//Develop and enhance existing tools and automation framework
+//Troubleshoot issues down to the root cause and provide detailed bug reports.
+//Detail oriented, highly adaptable and acts as a customer advocate for quality.
+//Work closely with stakeholders, communicate with team about any  issues, risks with the application in test.
+//The Must Haves:
+//
+//6+ years of experience in software testing and automation
+//Strong experience in developing test automation using Python or similar.
+//Proven experience in all areas of product testing
+//Experience in building reliable  automation framework and test tools.
+//Experience using Docker and containerized architectures
+//Experience in Continuous integration (CI) and continuous delivery (CD) such as Gitlab, Jenkins.
+//Experience with open source tools and resources
+//The Nice-to-Haves:
+//
+//Experience of geospatial concepts
+//Experience in Agile development environment
 
         return (
             <div>
@@ -27,7 +49,7 @@ class Work extends Component {
                         <CardContent>
                           <Typography gutterBottom variant="h5" component="h2">
 //                            Title
-                            Lizard
+                            Software Quality Engineer, Intern | June 2019 - Present
                           </Typography>
                           <Typography variant="body2" color="textSecondary" component="p">
 //                            Description
