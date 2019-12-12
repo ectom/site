4443f9cfa75d4ed367a2e033f5252bc5f8813384
Ethan Tom <ethan@planet.com>
2019-12-11 16:21:55 -0800

    added MUI links. not sure if they work yet

--- client/src/pages/Social.js
+++ client/src/pages/Social.js
@@ -8,10 +8,38 @@ class Social extends Component {
     render() {
         return (
 //            TODO add links to each profile
-            <GitHubIcon/>
-            <LinkedInIcon/>
-            <InstagramIcon/>
-            <EmailIcon/>
+            <Link
+                to="https://www.github.com/ectom"
+                target="_blank"
+                rel="noopener"
+//                url: 'https://twitter.com/intent/tweet',
+//                query: {
+//                    url,
+//                },
+            >
+                <GitHubIcon/>
+            </Link>
+            <Link
+                to="https://www.linkedin.com/in/ectom"
+                target="_blank"
+                rel="noopener"
+            >
+                <LinkedInIcon/>
+            </Link>
+            <Link
+                to="https://www.instagram.com/ethanchristophertom"
+                target="_blank"
+                rel="noopener"
+            >
+                <InstagramIcon/>
+            </Link>
+            <Link
+                href={`mailto:?${query}`}
+                target="_blank"
+                rel="noopener"
+            >
+                <EmailIcon/>
+            </Link>
         )
     }
 }
