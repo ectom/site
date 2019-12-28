ca2412740235f55d8e469fc61736efa472d524da
Ethan Tom <etom579@gmail.com>
2019-12-27 19:42:16 -0800

    express removed. file structure changed

--- api/config/routes/routes.js
+++
@@ -1,7 +0,0 @@
-// const path = require('path');
-const controller = require('../../controllers/controller');
-
-module.exports = function(app){
-  app.get('/api/hello', controller.hello);
-  app.post('/api/contactMe', controller.contactMe);
-}
