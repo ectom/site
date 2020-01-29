f72952ed5627f6267b615d14ccd1869d2b56d259
Ethan Tom <etom579@gmail.com>
2020-01-29 01:46:35 -0800

    Updates

---
+++ precache-manifest.a04da21c4ec07bf3113dacc81e38a44f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b6ec14e52ef523561940f87f80e405e",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "d2bb898dc521914eb958",
+    "url": "/site/static/css/main.42bcd9f7.chunk.css"
+  },
+  {
+    "revision": "c430e754432811470ba7",
+    "url": "/site/static/js/2.6144d673.chunk.js"
+  },
+  {
+    "revision": "a58d60b4914e1164d175ad7f57a8d682",
+    "url": "/site/static/js/2.6144d673.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d2bb898dc521914eb958",
+    "url": "/site/static/js/main.7d86430b.chunk.js"
+  },
+  {
+    "revision": "de467bcccfae26b472e6",
+    "url": "/site/static/js/runtime-main.ebb8f33b.js"
+  },
+  {
+    "revision": "6e4a9679e65cc320746c3e5d48e51f28",
+    "url": "/site/static/media/Raleway-Regular.6e4a9679.ttf"
+  }
+]);
